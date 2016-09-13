afd373358d22acf9e884f3a28f589689510fba46
Alonso Sanchez <alonso.j.sanchez@oracle.com>
2016-09-12 21:17:55 -0500

    Integration and Method info forms added.

--- js/modules/forms/indicator.js
+++ js/modules/forms/indicator.js
@@ -2,12 +2,15 @@ define(
         [
             'knockout', 'ojs/ojcore', 'ojs/ojknockout', 'ojs/ojtabs',
             'ojs/ojinputtext', 'ojs/ojselectcombobox',
-            'ojs/ojdatetimepicker'
+            'ojs/ojdatetimepicker', 'ojs/ojtable', 'ojs/ojarraytabledatasource'
         ],
         function (ko, oj) {
             function IndicatorViewModel() {
                 this.startDate = ko.observable(oj.IntlConverterUtils.dateToLocalIso(new Date()));
                 this.lastUpdate = ko.observable(oj.IntlConverterUtils.dateToLocalIso(new Date()));
+                
+                this.selectedProcesses = ko.observable(new oj.ArrayTableDataSource([]));
+                this.selectedProjects = ko.observable(new oj.ArrayTableDataSource([]));
             }
 
             return IndicatorViewModel;
