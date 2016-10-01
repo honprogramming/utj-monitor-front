cb1d888a28b52308fd9c272f2c289c86948f1708
Alonso Sanchez <alonso.j.sanchez@oracle.com>
2016-09-30 22:18:59 -0500

    First tab ready. Event validation when switching tabs.

--- js/modules/forms/profile.js
+++ js/modules/forms/profile.js
@@ -12,10 +12,25 @@ define(
 
                 self.id = "profile-tab";
                 self.name = ko.observable();
+                self.lastName = ko.observable();
+                self.secondLastName = ko.observable();
+                self.phoneNumber = ko.observable();
+                self.email = ko.observable();
+                self.studyLevel = ko.observable();
+                self.studyStatus = ko.observable();
+                self.area = ko.observable();
+                self.subArea = ko.observable();
+                self.position = ko.observable();
+                self.manager = ko.observable();
+                self.emailUTJ = ko.observable();
+                self.phoneUTJ = ko.observable();
+                self.extension = ko.observable();
+                self.joinDate = ko.observable();
+
                 self.title = GeneralViewModel.nls("forms.profile.title");
                 self.personalInfoTabTitle = GeneralViewModel.nls("forms.profile.tabs.personal-info.title");
                 self.utjInfoTabTitle = GeneralViewModel.nls("forms.profile.tabs.utj-info.title");
-                
+
                 self.nameLabel = GeneralViewModel.nls("forms.profile.tabs.personal-info.name");
                 self.lastNameLabel = GeneralViewModel.nls("forms.profile.tabs.personal-info.lastName");
                 self.secondLastNameLabel = GeneralViewModel.nls("forms.profile.tabs.personal-info.secondLastName");
@@ -23,7 +38,7 @@ define(
                 self.emailLabel = GeneralViewModel.nls("forms.profile.tabs.personal-info.email");
                 self.studyLevelLabel = GeneralViewModel.nls("forms.profile.tabs.personal-info.studyLevel");
                 self.studyLevelStatusLabel = GeneralViewModel.nls("forms.profile.tabs.personal-info.studyLevelStatus");
-                
+
                 self.areaLabel = GeneralViewModel.nls("forms.profile.tabs.utj-info.area");
                 self.subAreaLabel = GeneralViewModel.nls("forms.profile.tabs.utj-info.subArea");
                 self.positionLabel = GeneralViewModel.nls("forms.profile.tabs.utj-info.position");
@@ -32,28 +47,51 @@ define(
                 self.phoneUTJLabel = GeneralViewModel.nls("forms.profile.tabs.utj-info.phone");
                 self.extLabel = GeneralViewModel.nls("forms.profile.tabs.utj-info.ext");
                 self.joinDateLabel = GeneralViewModel.nls("forms.profile.tabs.utj-info.joinDate");
-                        
+
+                self.messageSummary = GeneralViewModel.nls("forms.validation.messageSummary");
+                self.emailPlaceHolder = GeneralViewModel.nls("forms.validation.email");
+                self.phonePlaceHolder = GeneralViewModel.nls("forms.validation.phone");
+                self.comboboxPlaceHolder = GeneralViewModel.nls("forms.combobox.placeholder");
+
                 self.nextButtonLabel = GeneralViewModel.nls("forms.buttons.next");
                 self.previousButtonLabel = GeneralViewModel.nls("forms.buttons.previous");
                 self.saveButtonLabel = GeneralViewModel.nls("forms.buttons.save");
-                
+
                 self.startDate = ko.observable(oj.IntlConverterUtils.dateToLocalIso(new Date()));
-                self.tracker = ko.observable();
+
+                self.trackers = [ko.observable(), ko.observable()];
+
+                self.selectionHandler = function (event, ui) {
+                    if (ui.originalEvent) {
+                        var currentTab = getCurrentTab.call(self);
+
+                        return !validate(getTabTracker.call(self, currentTab));
+                    }
+                };
 
                 self.relative = function (tabsNumber) {
-                    if (!validate()) {
-                        var currentTab = $("#" + self.id).ojTabs("option", "selected");
+                    var currentTab = getCurrentTab.call(self);
+
+                    if (!validate(getTabTracker.call(self, currentTab))) {
                         $("#" + self.id).ojTabs("option", "selected", currentTab + tabsNumber);
                     }
                 };
 
-                function validate() {
-                    var tracker = ko.unwrap(self.tracker);
+                function getCurrentTab() {
+                    return  $("#" + this.id).ojTabs("option", "selected");
+                }
+
+                function getTabTracker(tabNumber) {
+                    return this.trackers[tabNumber];
+                }
+
+                function validate(observableTracker) {
+                    var tracker = ko.unwrap(observableTracker);
                     tracker.showMessages();
                     return tracker.focusOnFirstInvalid();
                 }
             }
-            
+
             return ProfileViewModel;
         }
 );
--- js/resources/nls/translations.js
+++ js/resources/nls/translations.js
@@ -1,95 +1,104 @@
-define({
-    root: {
-        "appTitle": "UTJ Monitor",
-        "header": {},
-        "home": {
-            "description": "Es una heramienta didáctica para realizar todo tipo de planes como planes de desarrollo, \n\
+define(
+        {
+            root: {
+                "appTitle": "UTJ Monitor",
+                "header": {},
+                "home": {
+                    "description": "Es una heramienta didáctica para realizar todo tipo de planes como planes de desarrollo, \n\
                             planes de negocio, planes personales, planes profesionales o cualquier tipo de plan que se requiera\n\
                             y lo más importante: No necesitas conocimientos previos de planeación para utilizar la herramienta ya que \n\
                             aprenderás los conceptos mientra vas avanzando en tu plan.",
-            "demoTitle": "Video explicativo",
-            "membershipsTitle": "Membresías",
-            "membershipsTable": {
-                "headers": {
-                    "permissions": "Permisos",
-                    "freeMembership": "Membresia Gratuita",
-                    "silverMembership": "Membresia Silver",
-                    "goldMembership": "Membresia Gold"
-                },
-                "permissions": {
-                    "15DaysTest1Plan": "Prueba 15 dias 1 Plan",
-                    "shareAndExportPlan": "Compartir y exportar Plan",
-                    "createUpTo5Plans": "Crear hasta 5 Planes",
-                    "acceptColaborators": "Aceptar colaboradores",
-                    "acceptAdministrators": "Aceptar administradores",
-                    "createUpTo10Plans": "Crear hasta 10 Planes"
-                },
-                "seeMore": "Ver más"
-            },
-            "contactTitle" : "Contacto",
-            "contactDescription" : "Aqui van los datos de contacto",
-            "crewTitle" : "Nuestro equipo",
-            "crewTableSeeMore": "Ver más"
-        },
-        "footer": {
-            "copyright": "Copyright &copy; 2016 UTJ Monitor and/or its affiliates. All rights reserved"
-        },
-        "controlPanel": {
-            "sunburst": {
-                "title": "Cumplimiento de metas PIDE"
-            },
-            "details": {
-                "title": "Detalles de cumplimiento de metas PIDE"
-            },
-            "vision": "Visión",
-            "axe": "Eje",
-            "axes": "Ejes",
-            "theme": "Tema",
-            "themes": "Temas",
-            "objective": "Objetivo",
-            "objectives": "Objetivos",
-            "strategy": "Estrategia",
-            "strategies": "Estrategias",
-            "indicator": "Indicador",
-            "indicators": "Indicadores",
-            "project": "Proyecto",
-            "projects": "Proyectos",
-            "product": "Producto",
-            "products": "Productos"
-        },
-        "forms": {
-            "profile": {
-                "title": "Nueva Ficha de Perfil de Puesto",
-                "tabs": {
-                    "personal-info": {
-                        "title": "Información Personal",
-                        "name": "Nombre(s)",
-                        "lastName": "Apellido Paterno",
-                        "secondLastName": "Apellido Materno",
-                        "phone": "Teléfono de contacto",
-                        "email": "Correo personal",
-                        "studyLevel": "Último grado de estudios",
-                        "studyLevelStatus": "Status de último grado de estudios"
+                    "demoTitle": "Video explicativo",
+                    "membershipsTitle": "Membresías",
+                    "membershipsTable": {
+                        "headers": {
+                            "permissions": "Permisos",
+                            "freeMembership": "Membresia Gratuita",
+                            "silverMembership": "Membresia Silver",
+                            "goldMembership": "Membresia Gold"
+                        },
+                        "permissions": {
+                            "15DaysTest1Plan": "Prueba 15 dias 1 Plan",
+                            "shareAndExportPlan": "Compartir y exportar Plan",
+                            "createUpTo5Plans": "Crear hasta 5 Planes",
+                            "acceptColaborators": "Aceptar colaboradores",
+                            "acceptAdministrators": "Aceptar administradores",
+                            "createUpTo10Plans": "Crear hasta 10 Planes"
+                        },
+                        "seeMore": "Ver más"
                     },
-                    "utj-info": {
-                        "title": "Información UTJ",
-                        "area": "Area",
-                        "subArea": "Sub-Area",
-                        "position": "Cargo",
-                        "manager": "Superior Inmediato",
-                        "email": "Correco Electrónico UTJ",
-                        "phone": "Teléfono UTJ",
-                        "ext": "Extensión",
-                        "joinDate": "Fecha de Ingreso"
+                    "contactTitle": "Contacto",
+                    "contactDescription": "Aqui van los datos de contacto",
+                    "crewTitle": "Nuestro equipo",
+                    "crewTableSeeMore": "Ver más"
+                },
+                "footer": {
+                    "copyright": "Copyright &copy; 2016 UTJ Monitor and/or its affiliates. All rights reserved"
+                },
+                "controlPanel": {
+                    "sunburst": {
+                        "title": "Cumplimiento de metas PIDE"
+                    },
+                    "details": {
+                        "title": "Detalles de cumplimiento de metas PIDE"
+                    },
+                    "vision": "Visión",
+                    "axe": "Eje",
+                    "axes": "Ejes",
+                    "theme": "Tema",
+                    "themes": "Temas",
+                    "objective": "Objetivo",
+                    "objectives": "Objetivos",
+                    "strategy": "Estrategia",
+                    "strategies": "Estrategias",
+                    "indicator": "Indicador",
+                    "indicators": "Indicadores",
+                    "project": "Proyecto",
+                    "projects": "Proyectos",
+                    "product": "Producto",
+                    "products": "Productos"
+                },
+                "forms": {
+                    "profile": {
+                        "title": "Nueva Ficha de Perfil de Puesto",
+                        "tabs": {
+                            "personal-info": {
+                                "title": "Información Personal",
+                                "name": "Nombre(s)",
+                                "lastName": "Apellido Paterno",
+                                "secondLastName": "Apellido Materno",
+                                "phone": "Teléfono de contacto",
+                                "email": "Correo personal",
+                                "studyLevel": "Último grado de estudios",
+                                "studyLevelStatus": "Estado de último grado de estudios"
+                            },
+                            "utj-info": {
+                                "title": "Información UTJ",
+                                "area": "Area",
+                                "subArea": "Sub-Area",
+                                "position": "Cargo",
+                                "manager": "Superior Inmediato",
+                                "email": "Correco Electrónico UTJ",
+                                "phone": "Teléfono UTJ",
+                                "ext": "Extensión",
+                                "joinDate": "Fecha de Ingreso"
+                            }
+                        }
+                    },
+                    "buttons": {
+                        "next": "Siguiente",
+                        "previous": "Anterior",
+                        "save": "Guardar"
+                    },
+                    "validation": {
+                        "messageSummary": "Valor Requerido",
+                        "email": "nombre@dominio.com",
+                        "phone": "33 3333 3333"
+                    },
+                    "combobox": {
+                        "placeholder": "Selecciona una opción"
                     }
                 }
-            },
-            "buttons": {
-                "next": "Siguiente",
-                "previous": "Anterior",
-                "save": "Guardar"
             }
         }
-    }
-}
 );
