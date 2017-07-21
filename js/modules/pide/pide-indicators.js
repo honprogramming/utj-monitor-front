131f8c69ed6ea78e83a7186a6dc65c0765e02bb3
Alonso Sanchez <asanchez@homeaway.com>
2017-07-21 01:01:12 -0500

    Indicators selection panel layout improvement

--- js/modules/pide/pide-indicators.js
+++ js/modules/pide/pide-indicators.js
@@ -26,7 +26,7 @@ define(
                 self.arrowClass = ko.observable(arrowClassStart + left);
                 self.displayPanel = ko.observable(true);
                 self.selectedNodes = ko.observableArray();
-                
+
                 self.toggleDrawer = function () {
                     self.arrowClass(arrowClassStart +
                             (self.arrowClass().includes(left) ? right : left));
@@ -67,22 +67,40 @@ define(
 
                 self.clickHandler = function (event, ui) {
                     var option = ui.option;
-                    self.selectedNodes([]);
-                    
+
                     if (option === "selection") {
                         var node = ui.value[0];
-                        var isSelected = node.type.includes("selected");
-                        var type = "indicator";
 
-                        if (!isSelected) {
-                            type += "-selected";
+                        if (node && node.type) {
+                            self.selectedNodes([]);
+                            var isSelected = node.type.includes("selected");
+                            var type = "indicator";
+
+                            if (!isSelected) {
+                                type += "-selected";
+                            }
+
+                            //  Add new node before initial node
+//                            $("#tree").ojTree("setType", node, type);
+                            node.type = type;
+                            
+                            var targetClass = " fa-";
+
+                            if (node.type.includes("selected")) {
+                                targetClass += "check-";
+                            }
+
+                            targetClass += "square-o";
+
+                            var regExp = /fa-\S*square-o/;
+
+                            if (node.className.match(regExp)) {
+                                node.className = node.className.replace(regExp, targetClass);
+                            } else {
+                                node.className += targetClass;
+                            }
                         }
-
-                        //  Add new node before initial node
-                        $("#tree").ojTree("setType", node, type);
                     }
-                    
-                    self.loadHandler();
                 };
 
                 self.loadHandler = function (event, ui) {
@@ -97,133 +115,24 @@ define(
                         node.childNodes.forEach(addNode);
 
                         if (node.type && node.type.includes("indicator")) {
-                            if (!node.className.includes("fa")) {
-                                node.className += " fa fa-lg";
-                            }
-                            
-                            var targetClass = " fa-";
-
-                            if (node.type.includes("selected")) {
-                                targetClass += "check-";
-                            }
-
-                            targetClass += "square-o";
-                            
-                            var regExp = /fa-\S*square-o/;
-                            
-                            if (node.className.match(regExp)) {
-                                node.className = node.className.replace(regExp, targetClass);
-                            } else {
-                                node.className += targetClass;
-                            }
-                            
+                            node.className += " fa fa-square-o";
                             node.style.display = "block";
                         }
                     }
-
 
                     function addNode(node) {
                         nodesArray.push(node);
                     }
                 };
-
-                var jo = [
-                    {
-                        "title": "Eje1",
-                        "attr": {"id": "eje1"},
-                        "children": [
-                            {
-                                "title": "Objetivo 1",
-                                "attr": {"id": "objetivo1"},
-                                "children": [
-                                    {
-                                        "title": "Indicador 1.1",
-                                        "attr": {
-                                            "id": "indicador1.1",
-                                            "type": "indicator-selected"
-                                        }
-                                    },
-                                    {
-                                        "title": "Indicador 1.2",
-                                        "attr": {
-                                            "id": "indicador1.2",
-                                            "type": "indicator-selected"
-                                        }
-                                    },
-                                    {
-                                        "title": "Indicador 1.3",
-                                        "attr": {"id": "indicador1.3",
-                                            "type": "indicator-selected"
-                                        }
-                                    }
-                                ]
-                            },
-                            {
-                                "title": "Objetivo 2",
-                                "attr": {"id": "objetivo2"},
-                                "children": [
-                                    {
-                                        "title": "Indicador 2.1",
-                                        "attr": {
-                                            "id": "indicador2.1",
-                                            "type": "indicator-selected"
-                                        }
-                                    },
-                                    {
-                                        "title": "Indicador 2.2",
-                                        "attr": {
-                                            "id": "indicador2.2",
-                                            "type": "indicator"
-                                        }
-                                    },
-                                    {
-                                        "title": "Indicador 2.3",
-                                        "attr": {
-                                            "id": "indicador2.3",
-                                            "type": "indicator"
-                                        }
-                                    }
-                                ]
-                            }
-                        ]
-                    },
-                    {
-                        "title": "Eje 2",
-                        "attr": {"id": "eje2"},
-                        "children": [
-                            {
-                                "title": "Objetivo 3",
-                                "attr": {"id": "objetivo3"},
-                                "children": [
-                                    {
-                                        "title": "Indicador 3.1",
-                                        "attr": {
-                                            "id": "indicador3.1",
-                                            "type": "indicator"
-                                        }
-                                    },
-                                    {
-                                        "title": "Indicador 3.2",
-                                        "attr": {
-                                            "id": "indicador3.2",
-                                            "type": "indicator-selected"
-                                        }
-                                    },
-                                    {
-                                        "title": "Indicador 3.3",
-                                        "attr": {
-                                            "id": "indicador3.3",
-                                            "type": "indicator"
-                                        }
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ];
+                var jo = [];
 
                 self.getJson = function (node, fn) {
-                    fn(jo);  // pass to ojTree using supplied function
+                    $.getJSON("data/pide-indicators.json").then(
+                            function (data) {
+                                jo = data;
+                                fn(jo);  // pass to ojTree using supplied function
+                            }
+                    );
                 };
 
                 self.getTypes = function () {
