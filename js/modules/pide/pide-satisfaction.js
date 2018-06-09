8b7c3c73a4fe548b72f6884af5492a834cdd1780
Alonso Sanchez <alonsocucei@hotmail.com>
2018-06-08 23:26:21 -0500

    Comment static json file

--- js/modules/pide/pide-satisfaction.js
+++ js/modules/pide/pide-satisfaction.js
@@ -38,8 +38,8 @@ define(
                         () => {
                             const controlPanelModel = new PIDEModel(controlPanelDataProvider);
                             
-//                            const indicatorsPromise = AjaxUtils.ajax(RESTConfig.indicators.pide.active.path);
-                            const indicatorsPromise = AjaxUtils.ajax("data/sunburst-utj.json");
+                            const indicatorsPromise = AjaxUtils.ajax(RESTConfig.indicators.pide.active.path);
+//                            const indicatorsPromise = AjaxUtils.ajax("data/sunburst-utj.json");
                             
                             indicatorsPromise.then(
                                 indicators => {
