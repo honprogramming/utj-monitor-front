ff943dd77e8a892a97620fbbf0166aa205084d02
Alonso Sanchez <alonsocucei@hotmail.com>
2018-11-30 21:07:55 -0600

    remove console log calls

--- js/utils/IdGenerator.js
+++ js/utils/IdGenerator.js
@@ -5,11 +5,8 @@ define(
                 let newId;
                 let scaleFactor = scale || idsArray.length * 2;
                 
-                console.debug("ids: %o", idsArray);
-                
                 do {
                     newId = Math.floor(Math.random() * scaleFactor + 1);
-                    console.debug("newId: %d", newId);
                 } while (idsArray.includes(newId.toString()));
                 
                 return newId;
